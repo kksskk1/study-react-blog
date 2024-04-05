e89d289c117a98944825aaabed58017fa37496d1
kksskk1 <doerksk@naver.com>
2024-04-05 14:24:16 +0900

    header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,9 @@
-
+import logo from '../../logo.svg';
 
 function Header() {
   return (
-    <div>
-        Header
+    <div className='relative w-full h-64 overflow-hidden'>
+        <img className='absolute inset-0 m-auto blur-lg' src={logo}/>
     </div>
   );
 }
