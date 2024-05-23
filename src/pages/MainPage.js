340ac0c82c2b1192c3f2d57efd5c7c2a0cb35b47
kksskk1 <doerksk@naver.com>
2024-05-23 16:47:22 +0900

    react query

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,18 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+
 import Layout from '../components/layout/Layout';
 import Content from '../components/layout/Content';
 
 function MainPage(props) {
+    const fetchPosts = async () => await axios.get(`/`).then(res => res.data);
+    const { data, isLoading, error } = useQuery(['fetchPosts'], fetchPosts);
+
+    console.log(data);
+    console.log(isLoading);
+    console.log(error);
+
     return (
         <Layout>
             <Content/>
